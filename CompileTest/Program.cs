f81a6539b9dcc86f92f12efc79712aa7414fcede
Heiko Milke <hm@knm.io>
2019-10-21 11:30:57 +0200

    wip

--- CompileTest/Program.cs
+++ CompileTest/Program.cs
@@ -30,18 +30,37 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
 
+            //https://luisfsgoncalves.wordpress.com/2017/03/20/referencing-system-assemblies-in-roslyn-compilations/
+
+            var trustedAssembliesPaths = ((string)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES")).Split(Path.PathSeparator);
+            foreach (var tp in trustedAssembliesPaths)
+            {
+                Console.WriteLine(tp);
+            }
+
+
         https://josephwoodward.co.uk/2016/12/in-memory-c-sharp-compilation-using-roslyn
 
             SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
 
 
-            var ass = new List<MetadataReference>();
-            ass.Add(MetadataReference.CreateFromFile(typeof(System.String).Assembly.Location));
-            ass.Add(MetadataReference.CreateFromFile(typeof(System.Linq.Enumerable).Assembly.Location));
-            ass.Add(MetadataReference.CreateFromFile(typeof(Console).Assembly.Location));
-            ass.Add(MetadataReference.CreateFromFile(typeof(IntPtr).Assembly.Location));
-            ass.Add(MetadataReference.CreateFromFile(typeof(Object).Assembly.Location));
 
+            var neededAssemblies = new[]
+{
+    "System.Runtime",
+    "mscorlib",
+};
+            var references = trustedAssembliesPaths
+                .Where(p => neededAssemblies.Contains(Path.GetFileNameWithoutExtension(p)))
+                .Select(p => MetadataReference.CreateFromFile(p))
+                .ToList();
+            var ass = new List<MetadataReference>(references);
+            ass.Add(MetadataReference.CreateFromFile(typeof(Object).Assembly.Location));
+            ass.Add(MetadataReference.CreateFromFile(typeof(System.Linq.Enumerable).Assembly.Location));
+             ass.Add(MetadataReference.CreateFromFile(typeof(System.String).Assembly.Location));
+             ass.Add(MetadataReference.CreateFromFile(typeof(Console).Assembly.Location));
+             ass.Add(MetadataReference.CreateFromFile(typeof(IntPtr).Assembly.Location));
+            // 
             var compile = Microsoft.CodeAnalysis.CSharp.CSharpCompilation.Create("fucker",
             options:
                 new CSharpCompilationOptions(OutputKind.ConsoleApplication)
