709bb6c8b860e9293fed6b56e902bfa242a4c626
Heiko Milke <hm@knm.io>
2019-10-21 09:45:23 +0200

    test console

---
+++ PerfTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using Mono.TextTemplating;
+
+namespace PerfTest
+{
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var gen = new TemplateGenerator();
+
+            Measure.Step();
+
+
+string outputFilename = "";
+
+            gen.ProcessTemplate(
+              null,
+              "hi",
+              ref outputFilename,
+              out var outputContent);
+            Measure.Step("Done processing");
+
+        }
+    }
+}
